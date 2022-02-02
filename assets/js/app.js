1f6982c5667f91fdb599306ac2c478ea1e75a4ef
AgathePons <ponsagathe37@gmail.com>
2022-02-02 13:43:57 +0100

    edit list name done

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,10 +3,9 @@ const app = {
   base_url: 'http://localhost:3000/',
   addListenerToAction: function() {
     document.getElementById('addListButton').addEventListener('click', app.showAddListModal);
-    document.querySelectorAll('.add-card-btn').forEach(btn => btn.addEventListener('click', app.showAddCardModal));
     document.querySelectorAll('.close').forEach(btn => btn.addEventListener('click', app.hideModals));
     document.querySelector('#addListModal form').addEventListener('submit', app.handleAddListForm);
-    //document.querySelector('#addCardModal form').addEventListener('submit', app.handleAddCardForm);
+    document.querySelector('#addCardModal form').addEventListener('submit', app.handleAddCardForm);
   },
   showAddListModal: function() {
     document.getElementById('addListModal').classList.add('is-active');
@@ -24,6 +23,17 @@ const app = {
     hiddenIdInput.setAttribute('value', listId);
     addCardModal.classList.add('is-active');
   },
+  showEditList: function(event) {
+    event.target.classList.add('is-hidden');
+    event.target.parentNode.querySelector('.edit-list-form').classList.remove('is-hidden');
+  },
+  hideEditList: function(listId) {
+    //!
+    console.log('clic hide edit list');
+    const list = document.querySelector(`.panel[data-list-id="${listId}"]`);
+    list.querySelector('.list-title').classList.remove('is-hidden');
+    list.querySelector('.edit-list-form').classList.add('is-hidden');
+  },
   handleAddListForm: async function(event) {
     event.preventDefault();
     const formData = new FormData(event.target);
@@ -33,11 +43,36 @@ const app = {
       body: formData
     });
     if(response.status === 201) {
-      app.makeListInDOM(formData);
+      const list = await response.json();
+      app.makeListInDOM(list);
     } else {
       console.log('post /lists something went wrond')
     }
     app.hideModals();
+  },
+  handleEditListForm: async function(event) {
+    event.preventDefault();
+    const listPanel = event.target.closest('.panel');
+    const listId = listPanel.dataset.listId;
+    const listTitle = listPanel.querySelector('.list-title');
+    try {
+      const formData = new FormData(event.target);
+      console.log('formData:', formData);
+      const response = await fetch(`${app.base_url}lists/${listId}`, {
+        method: 'PATCH',
+        body: formData
+      });
+      if(response.status === 200) {
+        const list = await response.json();
+        listTitle.textContent = list.name;
+        // on re hide le form
+        app.hideEditList(listId);
+      } else {
+        console.log('post /cards something went wrond')
+      }
+    } catch(error) {
+      console.error('handle edit list form:', error);
+    }
   },
   makeListInDOM: function(list) {
     // get and import template
@@ -48,36 +83,44 @@ const app = {
     const panel = cloneTemplate.firstElementChild;
     panel.dataset.listId = list.id;
     //TODO ou panel.setAttribute('data-list-id', list.id);
-    const listTitleH = cloneTemplate.getElementById('newListTitle');
-    listTitleH.textContent = list.name;
+    cloneTemplate.querySelector('.list-title').textContent = list.name;
     // set new event listener on new list
     cloneTemplate.querySelector('.add-card-btn').addEventListener('click', app.showAddCardModal);
+    cloneTemplate.querySelector('.list-title').addEventListener('dblclick', app.showEditList);
+    cloneTemplate.querySelector('.edit-list-form').addEventListener('submit', app.handleEditListForm);
     // append in DOM
     const kanbanBoard = document.getElementById('kabanBoard');
     kanbanBoard.appendChild(cloneTemplate);
   },
   handleAddCardForm: async function(event) {
     event.preventDefault();
-    const formData = new FormData(event.target);
-    const response = await fetch(`${app.base_url}cards`, {
-      method: 'POST',
-      body: formData
-    });
-    if(response.status === 201) {
-      app.makeCardInDOM(formData);
-    } else {
-      console.log('post /cards something went wrond')
+    try {
+      const formData = new FormData(event.target);
+      const response = await fetch(`${app.base_url}cards`, {
+        method: 'POST',
+        body: formData
+      });
+      if(response.status === 201) {
+        const card = await response.json();
+        app.makeCardInDOM(card);
+      } else {
+        console.log('post /cards something went wrond')
+      }
+      app.hideModals();
+    } catch(error) {
+      console.error('handle card form:', error);
     }
-    app.hideModals();
   },
   makeCardInDOM: function(card) {
     const template = document.getElementById('cardTemplate');
     const cloneTemplate = document.importNode(template.content, true);
+    const cardDOM = cloneTemplate.querySelector('.box');
     // set the title
     const cardTitleDiv = cloneTemplate.querySelector('.card-title');
     cardTitleDiv.textContent = card.content;
-    //set the id
-    cloneTemplate.querySelector('.box').dataset.cardId = card.id;
+    //set the id & color
+    cardDOM.dataset.cardId = card.id;
+    cardDOM.style.backgroundColor = card.color;
     // append in the list, in the card-container div
     const list = document.querySelector(`.panel[data-list-id="${card.list_id}"]`);
     const cardContainer = list.querySelector('.card-container');
@@ -91,15 +134,11 @@ const app = {
       });
       if(response.status === 200) {
         const lists = await response.json();
-        console.log(lists);
         // for each list
         for(const list of lists) {
           app.makeListInDOM(list);
           // for each card in a list
           for(const card of list.cards) {
-            /* const formDataCard = new FormData();
-            formDataCard.append('content', card.content);
-            formDataCard.append('list_id', card.list_id); */
             app.makeCardInDOM(card);
           }
         }
@@ -109,7 +148,6 @@ const app = {
     } catch(error) {
       console.error('get all lists:', error);
     }
-    
   },
   // ----------- INIT ----------- //
   init: function() {
